0f75e09d5986330b7ae343ffd9e84db7b737c013
stek ferney aguirre ceballos <stek-04@hotmail.com>
2023-09-26 21:27:51 -0500

    lista 3 controlador de login

--- index.php
+++ index.php
@@ -1,16 +1,14 @@
+
 <?php
-include("app/config.php")
+include ("app/config.php")
 ?>
+
 <!DOCTYPE html>
-<!--
-This is a starter template page. Use this page to start your new project from
-scratch. This page gets rid of all links and provides the needed markup only.
--->
 <html lang="en">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>login-sisventasc:\xampp\htdocs\sistemadeventas-muestra\login\index.php</title>
+  <title>Sistema de ventas</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
